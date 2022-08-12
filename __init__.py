62b0fc17722ccb8b892f24839be53d29b2115baa
Kelvin Lee <59814445+Kelvinthedrugger@users.noreply.github.com>
2022-08-13 04:16:34 +0800

    It's a silly joke!

---
+++ __init__.py
@@ -0,0 +1,1 @@
+print("Give me a star if you find this helpful, wouldn't you\nnah, for one to not print this annoying message anymore, simply remove this line/file(__init__.py) from your folder and you're cool")
